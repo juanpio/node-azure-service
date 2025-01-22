cb35a7a619c58a7dfd2404a8266d9eaf143ff1d3
Juan <pio.juanandres@gmail.com>
2025-01-22 18:43:37 -0500

    Try to implement a initializador

--- commands.sh
+++ commands.sh
@@ -5,14 +5,14 @@ export CONFIG_PATH_SB='../node-azure-services/service-bus/config/Config.json'
 export CONFIG_PATH_EH='../node-azure-services/event-hub/config/Config.json'
 export COMPOSE_DOWN=n
 
-export SERVICEBUS_CONNECTION_STRING="Endpoint=sb://servicebus-emulator;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;"
-export EVENTHUB_CONNECTION_STRING="Endpoint=sb://eventhubs-emulator;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;"
-export EVENTHUB_NAME=iep-claim-ingetion
-export PATH_INIT_DATA_FILE='path_to_your_file.csv'
+export SERVICEBUS_CONNECTION_STRING="Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;"
+export EVENTHUB_CONNECTION_STRING="Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;"
+export EVENTHUB_NAME="eventhub"
+export PATH_INIT_DATA_FILE="path_to_your_file.csv"
 
 env
 #Run docker compose down
-#docker compose -f "azure-service-bus-emulator-installer/Sample-Code-Snippets/docker-compose-js.yml" down --volumes --remove-orphans
+#docker compose -f "azure-service-bus-emulator-installer/Sample-Code-Snippets/docker-compose-js.yml" down
 
 #Run docker compose file detached mode
 docker compose -p event-services-compouse -f "node-azure-services/docker-compose-js.yml" up -d --build
--- initializator/main.js
+++ initializator/main.js
@@ -1,7 +1,10 @@
 // Implementation of a service that will send messages to a EnventHub, from a file in the current directory with the name 'path_to_your_file.csv' written in javascript.
 // The file should have the following format: id,data1,data2,data3
 
-const { EventHubProducerClient } = require("@azure/event-hubs");
+const {
+  EventHubProducerClient,
+  EventHubConsumerClient,
+} = require("@azure/event-hubs");
 const fs = require("fs");
 const path = require("path");
 
@@ -11,19 +14,23 @@ console.debug(process.env["PATH_INIT_DATA_FILE"]);
 
 const connectionString =
   "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
-const eventHubName = "iep-claim-ingetion";
+const namespace = "ns-claim-activity";
+const eventHubName = "eh-claim-activity";
+const consumerGroup = "cg-claim-ingestion";
+const partitionKey = "pk-claim-ingestion"; //"claim-activity";
+const messagingEntity = "me-claim-activity";
+
 const pathToFile = path.join(__dirname, "path_to_your_file.csv");
 
 async function main() {
-  const producer = new EventHubProducerClient(connectionString, eventHubName);
+  const producer = new EventHubProducerClient(connectionString, consumerGroup );
   //const batch = await producer.createBatch();
   try {
     // Create a batch of events
     const eventDataBatch = await producer.createBatch();
 
-    // Add a message to the batch with the partition key "entity1"
-    const message = { body: "Hello, this is a message to entity1!" };
-    const partitionKey = "eh1";//"claim-activity";
+    // Add a message to the batch with the partition key "eventHubName"
+    const message = { body: "Hello, this is a message to claim-ingestion!" };
 
     // Add the message to the batch
     if (!eventDataBatch.tryAdd({ body: message.body, partitionKey })) {
@@ -32,13 +39,53 @@ async function main() {
 
     // Send the batch of events to the Event Hub
     await producer.sendBatch(eventDataBatch);
-    console.log("Message sent successfully to entity1!");
+    console.log("Message sent successfully to eventHubName!");
   } catch (err) {
     console.error("Error sending message: ", err);
   } finally {
     // Close the producer
     await producer.close();
   }
+
+  // try {
+  //   // Read the events from the eventhub and print them
+  //   const consumer = new EventHubConsumerClient(
+  //     consumerGroup, //EventHubConsumerClient.defaultConsumerGroupName,
+  //     connectionString,
+  //     eventHubName
+  //   );
+
+  //   // Subscribe to the events
+  //   consumer.subscribe({
+  //     processEvents: async (events, context) => {
+  //       for (const event of events) {
+  //         console.log(
+  //           `Received event: '${event.body}' from partition: '${context.partitionId}' and consumer group: '${context.consumerGroup}'`
+  //         );
+  //       }
+  //     },
+  //     processError: async (err, context) => {
+  //       console.error(`Error : ${err}`);
+  //     },
+  //   });
+
+  //   // Wait for a few seconds
+  //   await new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, 10 * 1000);
+  //   });
+
+  //   // Close the consumer
+  //   await consumer.close();
+
+  //   console.log("Message read successfully to eventHubName!");
+  // } catch (err) {
+  //   console.error("Error sending message: ", err);
+  // } finally {
+  //   // Close the producer
+  //   await producer.close();
+  // }
 
   // const data =
   //     fs.readFileSync(pathToFile, 'utf8')
