3cde9a255134d39a9890976ee4f35508f60423c2
Juan <pio.juanandres@gmail.com>
2025-01-23 00:14:32 -0500

    Adjusting details after clean up secrets

--- commands.sh
+++ commands.sh
@@ -15,5 +15,5 @@ env
 #docker compose -f "azure-service-bus-emulator-installer/Sample-Code-Snippets/docker-compose-js.yml" down
 
 #Run docker compose file detached mode
-docker compose -p event-services-compouse -f "node-azure-services/docker-compose-js.yml" up -d --build
+docker compose -p event-services-compouse -f "docker-compose-js.yml" up -d --build
 
