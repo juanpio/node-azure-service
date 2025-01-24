ca38efe3eadc77b414ea4c8ca12b52bcf0e0cc7d
Juan <pio.juanandres@gmail.com>
2025-01-24 09:21:59 -0500

    Fixing connectivity issue between api container and db container

--- node-api-mongo/commands.sh
+++ node-api-mongo/commands.sh
@@ -1,6 +1,6 @@
 #set environment variables
 
-export DB_ATLAS_URI="mongodb://admin:pWwnymGCf5XEwNoK@mongodb:27017/iep-program-initialization?authSource=admin&directConnection=true"
+export DB_ATLAS_URI="mongodb://admin:pWwnymGCf5XEwNoK@172.24.0.2:27017/iep-program-initialization?authSource=admin&directConnection=true"
 export DB_ATLAS_MONGO_ADMIN_USER="admin"
 export DB_ATLAS_MONGO_ADMIN_PASSWORD="pWwnymGCf5XEwNoK"
 export ME_CONFIG_MONGODB_ADMINUSER="admin"
@@ -8,6 +8,8 @@ export ME_CONFIG_MONGODB_ADMINPASSWORD="pWwnymGCf5XEwNoK"
 export ME_CONFIG_MONGODB_SERVER="mongodb"
 
 env
+#Create a docker network for dbcontainer
+docker network create iep_emulator
 
 #Run docker compose file detached mode
 docker compose -p node-mongo-api-compouse -f "node-api-mongo/docker-compose.yml" up -d --build
